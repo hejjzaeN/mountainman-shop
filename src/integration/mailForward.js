3ee147e821579666dc570decd04383d5652cae24
hejjzaeN <pashokey@mail.ru>
2023-11-13 18:02:58 +0300

    comments

--- src/integration/mailForward.js
+++ src/integration/mailForward.js
@@ -2,18 +2,15 @@ import nodemailer from 'nodemailer'
 import ENV from '../env.js';
 
 async function main() {
-  // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     host: ENV.SMTP_SERVER,
     port: ENV.BREVO_PORT,
-    secure: false, // true for 465, false for other ports
+    secure: false,
     auth: {
       user: ENV.BREVO_LOGIN,
       pass: ENV.BREVO_PASSWORD,
     },
   });
-
-  // send mail with defined transport object
   let info = await transporter.sendMail({
     from: ENV.MAIL_FROM,
     to: ENV.MAIL_TO,
@@ -21,7 +18,7 @@ async function main() {
     text: 'Hello {{ contact.FIRSTNAME }} , This is an SMTP message with customizations',
   });
 
-  console.log('Message sent: %s', info.messageId);
+  console.log('Сообщение отправлено: %s', info.messageId);
 }
 
 main().catch(console.error);
